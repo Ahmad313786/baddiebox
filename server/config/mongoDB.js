5e61e4b1d3237add265e2df504931a73e1e0c8df
Ahmad Hassan <ahmhass313786@gmail.com>
2025-10-08 19:16:51 +0500

    mongo

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   if (isConnected) return;
 
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     isConnected = conn.connections[0].readyState;
     console.log("✅ MongoDB connected");
   } catch (error) {
