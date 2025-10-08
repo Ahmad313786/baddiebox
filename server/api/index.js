aed65bff1ede7d51b9e091264a3536919e59f3e4
Ahmad Hassan <ahmhass313786@gmail.com>
2025-10-08 19:30:43 +0500

    check

--- server/api/index.js
+++ server/api/index.js
@@ -1,32 +1,9 @@
 import express from "express";
-import cors from "cors";
-import "dotenv/config.js";
-import connectDB from "../config/mongoDB.js";
-import connectCloudinary from "../config/cloudinary.js";
-
-import userRouter from "../routes/userRoute.js";
-import productRouter from "../routes/productRoute.js";
-import cartRouter from "../routes/cartRoute.js";
-import orderRouter from "../routes/orderRoute.js";
-import adminRouter from "../routes/adminRoute.js";
-import otpRoutes from "../routes/otpRoute.js";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-connectCloudinary();
-connectDB();
-
-// Health check route
-app.get("/", (req, res) => res.send("✅ API is working on Vercel!"));
-
-// Routes
-app.use("/user", userRouter);
-app.use("/product", productRouter);
-app.use("/cart", cartRouter);
-app.use("/order", orderRouter);
-app.use("/admin", adminRouter);
-app.use("/otp", otpRoutes);
+app.get("/", (req, res) => {
+  res.send("✅ Simple API working!");
+});
 
 export default app;
